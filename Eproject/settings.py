e374aa89c37a0f817ffff1bbc6240a8637fb80e3
keerthinc <74804111+keerthinc@users.noreply.github.com>
2021-09-23 17:42:36 +0530

    Allowed host

--- Eproject/settings.py
+++ Eproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2boelk5ncc!n(+qs-@_tdc$fi^w@otrkbh(+yphae^9e^%'
+SECRET_KEY = 'django-insecure-pq@-2boelk5ncc!n(+qs-@_tdc$fi^w@otrkbh(+yphae^9e^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eeapp.herokuapp.com', '']
+ALLOWED_HOSTS = ['https://eeapp.herokuapp.com/']
 
 
 # Application definition
